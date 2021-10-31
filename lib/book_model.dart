9a6dca40229fee9f1852e8b1953e4594b9f968e7
Muhammad Abdullah Khan <abdullahkhan.smiu@gmail.com>
2021-10-31 18:53:41 +0000

    Changes Made

--- lib/book_model.dart
+++ lib/book_model.dart
@@ -1,33 +1,33 @@
-class ShoeModel {
+class AlbumModel {
   final String name;
   final String decs;
-  final String brand;
+  final String artist;
   final String imgPath;
 
-  ShoeModel(
+  AlbumModel(
       {required this.name,
       required this.decs,
-      required this.brand,
+      required this.artist,
       required this.imgPath});
-  static List<ShoeModel> list = [
-    ShoeModel(
+  static List<AlbumModel> list = [
+    AlbumModel(
         name: "DONDA",
         decs: "2021 relaesed Greatest of Album of the Year by the Greatest Artist" +
             " having Triology of WeekEnd, Lil Baby and Kanye and some dope story telling.",
-        brand: "Kanye West",
+        artist: "Kanye West",
         imgPath: "assets/cover 1.jpg"),
-    ShoeModel(
+    AlbumModel(
         name: "DAMN.",
         decs: "A unique album released in 2017 by one the Greatest Ever Kendrick Lamar(The Goat). This was " +
             "his Greatest Album got 24 nominations and Kendrick was awarded 4 Grammy's for this album ",
-        brand: "Kendric Lamar",
+        artist: "Kendric Lamar",
         imgPath: "assets/cover2.jpg"),
-    ShoeModel(
+    AlbumModel(
         name: "?",
         decs:
             "Some the saddest albums of this Genre. The reason behind the name is X is asking questions" +
                 "one of his closests freind killed himself.",
-        brand: "Xxxtentaction",
+        artist: "Xxxtentaction",
         imgPath: "assets/cover3.jpg"),
   ];
 }
